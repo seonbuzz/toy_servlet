d7f4e2141c34a1688236d4abf5d3a52a6a0bc018
seonbuzz <ddonsdevelop@gmail.com>
2022-12-15 09:29:54 +0900

    first commit

---
+++ src/main/java/com/ddonsdevelop/toy_servlet/ToyServletApplication.java
@@ -0,0 +1,13 @@
+package com.ddonsdevelop.toy_servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyServletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddonsdevelop/toy_servlet/ToyServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddonsdevelop.toy_servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
